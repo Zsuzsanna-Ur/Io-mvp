d6d6463164b02c20ae37e0daee8231c8afa13385
io <eieioart@gmail.com>
2023-08-28 15:40:54 -0400

    initialize database with two tables, one for recipes one for categories, and add initial data

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,21 +3,45 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists recipes;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
-    );
+CREATE TABLE `category`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+   `name` VARCHAR(255) NOT NULL
+);
 
-INSERT INTO students (firstname, lastname)
+
+CREATE TABLE `recipes`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `link` TINYINT NOT NULL,
+    `description` LONGTEXT NOT NULL,
+    `category_id` INT NOT NULL
+);
+
+
+ALTER TABLE 
+    `recipes` ADD CONSTRAINT `recipes_category_id_foreign` FOREIGN KEY(`category id`) REFERENCES `category`(`id`);
+
+INSERT INTO category (name)
+VALUES  
+    ('vegan'),
+    ('vegetarian')
+
+INSERT INTO recipes (name, link, description, category_id)
 VALUES
-    ('Sally', 'Sevilla'),
-    ('Bernie', 'Barcelona');
+    (`Lion's Mane Mushroom Crab Cakes`, 
+    https://www.yummymummykitchen.com/2021/03/lions-mane-mushroom-recipes-crab-cakes.html#mv-creation-558-jtr,
+    `These vegan crab cakes made with mushrooms are even tastier than the original. They are crispy on the outside and tender and meaty on the inside. `,
+    1),
+    (`Pupusas de Frijol con Queso`,
+    https://twodollareats.com/recipe/pupusas-de-frijol-con-queso/,
+    `Pupusas are the national food of one of my favorite places on earth, El Salvador. On every street corner, I heard the distinct pat-pat-pat of the women making the pupusas and just thinking of it brings me back! As it is basically illegal in El Salvador to eat these with a fork, be prepared to eat with your hands and get a little messy!`,
+    2)
+
+
