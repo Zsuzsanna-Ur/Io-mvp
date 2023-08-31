9470b89c242c4f61e7090b22a74862631bcf40ad
io <eieioart@gmail.com>
2023-08-31 18:58:53 -0400

    create recipeList function

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,24 +1,35 @@
 import React from "react";
+import Axios from "axios";
 import "./RecipeList.css";
 
 // state to store RecipeList
 // function to get recipes from backend
 // useEffect that triggers the function
 
-// function RecipeList(){
-//     return (
-//         <div className="RecipeList">
-//             {/* <ul>
-//                 {.recipes.map(r => (
-//                     <li key={r.id}>
-//                         <span onClick={e => props.showCB(r.id)}>
-//                             {r.name} {r.description} {r.category_id}
-//                         </span>
-//                     </li>
-//                 ))}
-//             </ul> */}
-//         </div>
-//     );
-// }
+function RecipeList(props) {
+  const [recipes, setRecipes] = React.useState([]); //set recipes will update state any time it's called and re-render
+
+  React.useEffect(() => {
+    Axios.get("http://localhost:4000/api/recipes").then((response) => {
+      //setting recipes to the response of the data
+      //not doing any error handling yet
+      setRecipes(response.data);
+    });
+  }, []); //will run any time dependencies change, leaving array empty because nothing needs to be added after it's initially fetched
+
+  return (
+    <div className="RecipeList">
+      <ul>
+        {recipes.map((r) => (
+          <li key={r.id}>
+            <span onClick={(e) => props.showCB(r.id)}>
+              {r.name} {r.description} {r.category}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default RecipeList;
